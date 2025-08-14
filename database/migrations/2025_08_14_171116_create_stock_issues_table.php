bd2076bc89c5d5bc68f40d7dbaea66b91ee9067c
Vun Thy <Vun.Thy@mjqe.edu.kh>
2025-08-14 17:30:22 +0700

    Update

---
+++ database/migrations/2025_08_14_171116_create_stock_issues_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('stock_issues', function (Blueprint $table) {
+            $table->id();
+            $table->date('issue_date');
+            $table->string('reference_no')->unique();
+            $table->unsignedBigInteger('warehouse_id');
+            $table->string('remarks')->nullable();
+            $table->string('approval_status')->default('Pending');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('warehouse_id')->references('id')->on('warehouses')->onDelete('restrict');
+            $table->foreign('created_by')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('updated_by')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('deleted_by')->references('id')->on('users')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('stock_issues');
+    }
+};
