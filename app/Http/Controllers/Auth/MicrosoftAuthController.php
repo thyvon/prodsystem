6e28ebb5a33278d9a34717f93f68442edb3cdb04
Vun Thy <Vun.Thy@mjqe.edu.kh>
2025-10-02 14:15:31 +0700

    Update

--- app/Http/Controllers/Auth/MicrosoftAuthController.php
+++ app/Http/Controllers/Auth/MicrosoftAuthController.php
@@ -26,28 +26,20 @@ class MicrosoftAuthController extends Controller
      */
     public function callback()
     {
-        // Get Microsoft user
         $microsoftUser = Socialite::driver('microsoft')->stateless()->user();
 
-        // Find or create user
         $user = User::updateOrCreate(
             ['email' => $microsoftUser->getEmail()],
             [
                 'name' => $microsoftUser->getName(),
                 'microsoft_id' => $microsoftUser->getId(),
-                'password' => bcrypt(Str::random(16)), // random password for new users
+                'password' => bcrypt(Str::random(16)),
+                'profile_url' => $microsoftUser->avatar, // save directly here
             ]
         );
 
-        // Save Microsoft profile URL if exists
-        if (!empty($microsoftUser->avatar)) {
-            $user->profile_url = $microsoftUser->avatar;
-            $user->save();
-        }
-
-        // Login the user
         Auth::login($user);
 
-        return redirect('/'); // or your dashboard
+        return redirect('/home');
     }
-}
+    }
