fb3970494e99c3c83b3320cb95ac662ac20659fc
thyvon <thyvonny@gmail.com>
2025-10-16 20:19:02 +0700

    Update

--- app/Services/SharePointService.php
+++ app/Services/SharePointService.php
@@ -108,7 +108,7 @@ class SharePointService
             'microsoft_token_expires_at' => null,
         ]);
 
-        Auth::logout();
+        Auth::guard('web')->logout();
         request()->session()->invalidate();
         request()->session()->regenerateToken();
 
