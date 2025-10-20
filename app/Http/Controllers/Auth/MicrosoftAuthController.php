884bb8c988ae09b78a7cd1890be02af0a4232849
Vun Thy <Vun.Thy@mjqe.edu.kh>
2025-10-20 09:28:48 +0700

    Update

--- app/Http/Controllers/Auth/MicrosoftAuthController.php
+++ app/Http/Controllers/Auth/MicrosoftAuthController.php
@@ -31,7 +31,7 @@ class MicrosoftAuthController extends Controller
                 'offline_access',
             ])
             ->with([
-                'prompt' => 'select_account login',
+                'prompt' => 'login'
             ])
             ->redirect();
     }
