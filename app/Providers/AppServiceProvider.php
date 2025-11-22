979d9a94c72557903803b0786c948d008b55aae7
thyvon <70102736+thyvon@users.noreply.github.com>
2025-11-22 13:11:19 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Spatie\LaravelPdf\Facades\Pdf;   // ← important
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,18 +19,5 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Fix Chrome sandbox error on shared hosting / production servers
-        // This makes spatie/laravel-pdf work perfectly everywhere
-        Pdf::configureBrowser(function (\Spatie\Browsershot\Browsershot $browsershot) {
-            $browsershot
-                ->noSandbox()
-                ->setOption('args', [
-                    '--disable-gpu',
-                    '--disable-dev-shm-usage',
-                    '--no-zygote',
-                    '--single-process',
-                    '--disable-setuid-sandbox',
-                ]);
-        });
     }
 }
